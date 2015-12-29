710f403dea337024278625f5991ba46dd999af91
Colton Anglin <colton01@icloud.com>
2015-12-29 02:50:13 -0600

    test font

--- Registry.swift
+++ Registry.swift
@@ -5,19 +5,25 @@ public class Registry {
 
   public let url = NSURL(string: "https://api.github.com/repos/colton/max/contents/.Registry")
 
-  func allFonts() {
+  public func allFonts() {
   let data = NSData(contentsOfURL: url!.URLByAppendingPathComponent("Fonts"))
   let json = JSON(data: data!)
   var i = 0
+  var fontsArray = []
   for _ in json {
-    print(json[i]["sha"])
     let fontData = NSData(contentsOfURL: NSURL(string: "https://raw.githubusercontent.com/Colton/Max/master/.Registry/Fonts/FiraMono-Regular.json")!)
     let fontJSON = JSON(data: fontData!)
-    if let download = fontJSON["url"].string {
+    if let _ = fontJSON["url"].string {
       let font = NSData(contentsOfURL: NSURL(string:fontJSON["url"].string!)!)
-      print(font!.sha256())
+      print(fontSHA(font!))
 }
     i++
   }
+  }
+
+  public func fontSHA(data : NSData) -> String {
+    let hash = data.sha256()
+    let shaString = "\(hash)".stringByTrimmingCharactersInSet(NSCharacterSet(charactersInString: "<>")).stringByReplacingOccurrencesOfString(" ", withString: "")
+    return shaString
   }
 }
