db6d895e06265cf0f8c85d7c3e0f77448541705c
Colton Anglin <colton01@icloud.com>
2015-12-31 17:35:13 -0600

    fix merge issue

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-sudo curl -o /usr/local/bin/max https://github.com/Colton/Max/releases/download/0.1/max
+curl -fsSL -o /usr/local/bin/max https://github.com/Colton/Max/releases/download/0.1/max
+chmod +x /usr/local/bin/max
 env -i
