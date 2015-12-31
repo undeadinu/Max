d2f8481cbe483fcb7f015f417b9ce2490d0df9f2
Colton Anglin <colton01@icloud.com>
2015-12-31 17:55:55 -0600

    updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 curl -fsSL -o /usr/local/bin/max https://github.com/Colton/Max/releases/download/0.1/max
 chmod +x /usr/local/bin/max
 env -i
-echo "\u001b Successfully installed Max. Type \"max\" to use it."
+echo "Successfully installed Max. Type \"max\" to use it."
